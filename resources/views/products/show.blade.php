d733459ec4d5d6f3bce0fda755500e5b1067c17e
Ntsakisir <36655961+Ntsakisir@users.noreply.github.com>
2019-07-15 06:25:34 -1100

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -19,8 +19,8 @@
                   <p><span>Description: </span>{{$products->description}}</p>
                     <p><span>Highest Bid:</span>{{$price}}</p>
                     <p><span>Average:</span>{{$avg}}</p>
-                    <p><span>Your Bid:</span>{{$avg}}</p>
                     <p><span>Views:</span> {{$products->views}}</p>
+                    <p><span>Your Bid:</span>{{$products->userb}}</p>
                   <hr>
                     </div>
                   </div>
@@ -42,32 +42,32 @@
                 
                   <h3 style="text-align:center;" class="card-header">{{ __('Place A Bid') }}</h3>
                   <div class="card-body">
-                  <form method="POST" action="{{url('/create')}}" aria-label="{{ __('Register') }}">
+                  <form method="POST" action="{{url('/create')}}" aria-label="{{ __('Bid') }}">
                           @csrf
   
                           <div class="form-group row">
-                              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                              <label for="u_email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
   
                               <div class="col-md-6">
-                                  <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                  <input id="u_email" type="u_email" class="form-control{{ $errors->has('u_email') ? ' is-invalid' : '' }}" name="u_email" value="{{ old('u_email') }}" required>
   
-                                  @if ($errors->has('email'))
+                                  @if ($errors->has('u_email'))
                                       <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $errors->first('email') }}</strong>
+                                          <strong>{{ $errors->first('u_email') }}</strong>
                                       </span>
                                   @endif
                               </div>
                           </div>
 
                           <div class="form-group row">
-                              <label for="price" class="col-md-4 col-form-label text-md-right">{{ __('Bid Price') }}</label>
+                              <label for="bid_price" class="col-md-4 col-form-label text-md-right">{{ __('Bid bid_price') }}</label>
   
                               <div class="col-md-6">
-                                  <input id="price" type="text" class="form-control{{ $errors->has('price') ? ' is-invalid' : '' }}" name="price" value="{{ old('price') }}" required autofocus>
+                                  <input id="bid_price" type="text" class="form-control{{ $errors->has('bid_price') ? ' is-invalid' : '' }}" name="bid_price" value="{{ old('bid_price') }}" required autofocus>
   
-                                  @if ($errors->has('price'))
+                                  @if ($errors->has('bid_price'))
                                       <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $errors->first('price') }}</strong>
+                                          <strong>{{ $errors->first('bid_price') }}</strong>
                                       </span>
                                   @endif
                               </div>
@@ -77,9 +77,9 @@
                                 <div class="col-md-6">
                                     <input hidden id="product_id" type="text" class="form-control{{ $errors->has('product_id') ? ' is-invalid' : '' }}" name="product_id" value="{{$products->id}}" required autofocus>
     
-                                    @if ($errors->has('price'))
+                                    @if ($errors->has('bid_price'))
                                         <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('price') }}</strong>
+                                            <strong>{{ $errors->first('bid_price') }}</strong>
                                         </span>
                                     @endif
                                 </div>
